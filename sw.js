73b636e455ac2aa09e37ac26f4acfd6dd7bbcc5b
BisUmTo <31419791+BisUmTo@users.noreply.github.com>
2022-09-23 23:19:25 +0200

    Offline PWA

--- sw.js
+++ sw.js
@@ -1,12 +1,10 @@
 // Change this to your repository name
 var GHPATH = '/pwa';
- 
-// Choose a different app prefix name
 var APP_PREFIX = 'pwae_';
  
-// The version of the cache. Every time you change any of the files
-// you need to change this version (version_01, version_02…). 
-var VERSION = 'version_002';
+// The version of the cache. 
+// Every time you change any file you need to change this version. 
+var VERSION = 'v0.0.3';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
@@ -16,3 +14,45 @@ var URLS = [
   `${GHPATH}/img/96x96.png`,
   `${GHPATH}/img/512x512.png`
 ]
+
+var CACHE_NAME = APP_PREFIX + VERSION
+self.addEventListener('fetch', function (e) {
+  console.log('Fetch request : ' + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { 
+        console.log('Responding with cache : ' + e.request.url);
+        return request
+      } else {       
+        console.log('File is not cached, fetching : ' + e.request.url);
+        return fetch(e.request)
+      }
+    })
+  )
+})
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Installing cache : ' + CACHE_NAME);
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('Deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
