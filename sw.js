b5b05d0ecedb60313bafce7b95da43e092518a23
BisUmTo <31419791+BisUmTo@users.noreply.github.com>
2022-09-23 18:10:48 +0200

    Added images to offline version

--- sw.js
+++ sw.js
@@ -2,15 +2,17 @@
 var GHPATH = '/pwa';
  
 // Choose a different app prefix name
-var APP_PREFIX = 'pwaexample_';
+var APP_PREFIX = 'pwae_';
  
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…). 
-var VERSION = 'version_00';
+var VERSION = 'version_001';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
   `${GHPATH}/`,
-  `${GHPATH}/index.html`
+  `${GHPATH}/index.html`,
+  `${GHPATH}/img/96x96.png`,
+  `${GHPATH}/img/512x512.png`
 ]
