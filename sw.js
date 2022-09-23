ad5b95acf54c353da71f7d4d4d5cd440e49b574e
BisUmTo <31419791+BisUmTo@users.noreply.github.com>
2022-09-23 18:23:20 +0200

    Added version to INDEX

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ var APP_PREFIX = 'pwae_';
  
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…). 
-var VERSION = 'version_001';
+var VERSION = 'version_002';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
